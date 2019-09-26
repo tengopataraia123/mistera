a1d144e1717d6b89d07d013ef8d40a6620b78f8c
tengopataraia123 <55848621+tengopataraia123@users.noreply.github.com>
2019-09-26 20:31:26 +0400

    Create 3-22.h

---
+++ 3-22.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+
+   /* Type your code here. */
+   
+   int color[] = {0,0,0};
+   int smallest = 255;
+   
+   scanf("%d %d %d",&color[0],&color[1],&color[2]);
+   
+   for(int i=0;i<3;i+=1){
+      if(color[i]<smallest){
+         smallest = color[i];
+      }
+   }
+   
+   printf("%d %d %d\n",color[0]-smallest,color[1]-smallest,color[2]-smallest);
+
+   return 0;
+}
