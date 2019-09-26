4625d99b24f161a251f453e454e4b3c9b7ac755f
tengopataraia123 <55848621+tengopataraia123@users.noreply.github.com>
2019-09-26 20:34:54 +0400

    Create 3-23.h

---
+++ 3-23.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+
+   /* Type your code here. */
+   
+   int nums[] = {0,0,0};
+   int max = 0;
+   
+   scanf("%d %d %d",&nums[0],&nums[1],&nums[2]);
+   
+   for(int i=0;i<3;i+=1){
+      if(nums[i]>max){
+         max = nums[i];
+      }
+   }
+   
+   printf("%d\n",max);
+
+   return 0;
+}
