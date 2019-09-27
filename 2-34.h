d7cc246459c103ea7df5d5b21a06cc1284dcaf79
tengopataraia123 <55848621+tengopataraia123@users.noreply.github.com>
2019-09-27 14:20:43 +0100

    Create 2-34.h

---
+++ 2-34.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+
+   /* Type your code here. Include the math library above first. */
+   
+   float frequency;
+   
+   scanf("%f",&frequency);
+   
+   for(float i=0;i<5;i+=1){
+      if(i == 4.0){
+         printf("%.2f\n",frequency*pow(2,i/12));
+      }else{
+      printf("%.2f ",frequency*pow(2,i/12));
+      }
+   }
+   
+   return 0;
+}
