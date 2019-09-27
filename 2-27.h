ad397ab836d0c041b43f4c3fe3ff26718047c1bf
tengopataraia123 <55848621+tengopataraia123@users.noreply.github.com>
2019-09-27 14:17:16 +0100

    Create 2-27.h

---
+++ 2-27.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+#define HALF 6
+
+int main(void) {
+   double caffeineMg;  // "double" supports floating-point like 75.5, versus int for integers like 75.
+   
+   scanf("%lf", &caffeineMg);
+
+   /* Type your code here. */
+   
+   for(int i=1;i<=3;i+=1){
+      printf("After %d hours: %.2f mg\n",HALF*i,caffeineMg/pow(2,i));
+   }
+   return 0;
+}
