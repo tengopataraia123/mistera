f40ba37b591a300e2fc6586f7f705fc7864409f2
tengopataraia123 <55848621+tengopataraia123@users.noreply.github.com>
2019-09-27 05:34:36 -0700

    Create lab2.h

---
+++ lab2.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+  char redId[20];
+  
+  scanf("%s",redId);
+  
+  int i =0;
+  while(redId[i] != '\0'){
+    printf("%c\n",redId[i]);
+    }
+   
+  return 0;
+}
