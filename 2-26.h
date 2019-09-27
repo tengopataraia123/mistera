54f9fd101fa5d6de5b884ec21204d680167922d5
tengopataraia123 <55848621+tengopataraia123@users.noreply.github.com>
2019-09-27 14:16:49 +0100

    Create 2-26.h

---
+++ 2-26.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+
+   /* Type your code here. */
+   
+   int userNum,x;
+   
+   scanf("%d %d",&userNum,&x);
+   
+   printf("%d %d %d %d\n",userNum/x,userNum/x/x,userNum/x/x/x,userNum/x/x/x/x);
+   return 0;
+}
